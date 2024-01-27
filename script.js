882d5582b5c6a1d1bb686277369961a4136b30c5
Harsh Chauhan <harsh2005.hc@gmail.com>
2024-01-27 13:52:22 +0530

    replaced files

---
+++ script.js
@@ -0,0 +1,6 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { x: "-100%", duration: 1, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 0.75 }, "-=1");
+tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 1 }, "-=1");
