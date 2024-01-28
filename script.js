a311a72d606cf02fa37164e222725132bc00d00c
SamarthaT <samarthat9@gmail.com>
2024-01-28 18:23:36 +0530

    Added responsive navbar

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.15 });
 tl.to(".slider", { x: "-100%", duration: 1, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 0.75 }, "-=1");
-tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 1 }, "-=1");
+tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 2 }, "-=1");
 
+function navResponsive() {
+    var x = document.getElementById("topnav");
+    if (x.className === "nav-collapse") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-collapse";
+    }
+} 
+
+function hamAnimate(x) {
+    x.classList.toggle("change");
+}
