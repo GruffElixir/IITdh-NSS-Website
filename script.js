ac3f85afcad2b2822efb2c141c447e8ce5ae3c89
SamarthaT <samarthat9@gmail.com>
2024-01-28 14:59:45 +0530

    Changed color and font

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { x: "-100%", duration: 1, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 0.75 }, "-=1");
 tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 1 }, "-=1");
+
