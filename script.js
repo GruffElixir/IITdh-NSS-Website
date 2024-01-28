695d008e7a775660249c91ae1a375974173b98af
Harsh Chauhan <harsh2005.hc@gmail.com>
2024-01-28 19:55:43 +0530

    new updates

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.15 });
 tl.to(".slider", { x: "-100%", duration: 1, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 0.75 }, "-=1");
-tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 2 }, "-=1");
+//tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 2 }, "-=1");
 
 function navResponsive() {
     var x = document.getElementById("topnav");
