7dfa6e3ffd84eaf0837333a44d6ffa13dccee7f7
Harsh Chauhan <harsh2005.hc@gmail.com>
2024-04-11 15:28:20 +0530

    gallery, events

--- script.js
+++ script.js
@@ -156,3 +156,12 @@ window.addEventListener('scroll', function counterView(event) {
     countAnim();
   }
 }, false);
+/*********code for parallax */
+$(document).ready(function () {
+  $('.hover-div').hover(function () {
+      $('.hover-div').stop().fadeTo('fast', 0.3);
+      $(this).stop().fadeTo('fast', 1);
+  }, function () {
+      $('.hover-div').stop().fadeTo('fast', 1);
+  });
+});
